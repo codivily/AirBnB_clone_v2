0d262c563bcd78596e9a24c7474ed2da82b64196
codivily <codivily@gmail.com>
2022-10-16 19:46:23 +0000

    models/state.py updated

--- models/state.py
+++ models/state.py
@@ -10,6 +10,10 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
 
     name = Column(String(128))
-    cities = relationship(
+    _cities = relationship(
             'City', back_populates='state',
             cascade='all, delete, delete-orphan')
+
+    @property
+    def cities(self):
+        return self._cities
